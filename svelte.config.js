76a212b08156a4e886637b351d050048dc65e204
leovoon <voonlihhaur@gmail.com>
2023-03-10 15:21:04 +0800

    [new] config preloadstrategy for  no waterfalls

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,26 @@
-import preprocess from "svelte-preprocess";
-import adapter from '@sveltejs/adapter-vercel';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import preprocess from 'svelte-preprocess'
+import adapter from '@sveltejs/adapter-vercel'
+import { vitePreprocess } from '@sveltejs/kit/vite'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: [vitePreprocess(), preprocess({
-        postcss: true
-    })],
+	preprocess: [
+		vitePreprocess(),
+		preprocess({
+			postcss: true
+		})
+	],
 
 	kit: {
 		adapter: adapter({
-            runtime: 'nodejs18.x'
-        })
+			runtime: 'nodejs18.x'
+		}),
+		output: {
+			preloadStrategy: 'preload-mjs'
+		}
 	}
-};
+}
 
-export default config;
+export default config
