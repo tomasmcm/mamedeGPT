374cee00bae0d1d5bf47ac37d82fa439d8acea97
Leo Voon <voonlihhaur@gmail.com>
2023-06-12 17:10:48 +0800

    Fixed back button container

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,4 @@
 <script>
-	import '../app.css'
 	import '../app.postcss'
 	import { page } from '$app/stores'
 	import { pwaInfo } from 'virtual:pwa-info'
@@ -41,21 +40,23 @@
 	</h1>
 	<slot />
 	{#if $page.route.id === '/about'}
-		<button class="h-full" on:click={() => history.back()}>
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				fill="none"
-				viewBox="0 0 24 24"
-				stroke-width="1.5"
-				stroke="currentColor"
-				class="w-6 h-6"
-			>
-				<path
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
-				/>
-			</svg>
-		</button>
+		<div class="h-full w-full grid place-items-center">
+			<button class="p-4" on:click={() => history.back()}>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					stroke="currentColor"
+					class="w-6 h-6"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
+					/>
+				</svg>
+			</button>
+		</div>
 	{/if}
 </div>
